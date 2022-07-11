0e3cb6d0949333f987dc0fa970ad7a4285f175fb
Mateus Carvalho <mtsbarbosa@hotmail.com>
2022-07-11 12:33:45 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.majorcluster/lein-template.diplomat-http-w-mysql-service "0.2.0"
   :description "Diplomat architecture-pedestal styled template with my sql db for leiningen generation"
-  :url "https://github.com/mtsbarbosa/diplomat-http-w-msysql-service-template"
+  :url "https://github.com/mtsbarbosa/diplomat-http-w-mysql-service-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
