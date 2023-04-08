aa0d4c05c221371337b8a79acc204cf668a3fba2
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-04-08 11:49:39 -0300

    Adding test workflow

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.majorcluster/lein-template.diplomat-http-w-mysql-service "0.4.1"
+(defproject org.clojars.majorcluster/lein-template.diplomat-http-w-mysql-service "0.4.2"
   :description "Diplomat architecture-pedestal styled template with my sql db for leiningen generation"
   :url "https://github.com/majorcluster/diplomat-http-w-mysql-service-template"
   :license {:name "MIT License"
